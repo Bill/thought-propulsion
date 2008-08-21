94d3963f0a149c468f0dd10459c1028165a33f99
Liopia <liopia@gmail.com>
2008-08-20 17:53:42 -0700

    changed blue color, made css use only dynamic color.

--- app/controllers/application_stylesheet_controller.rb
+++ app/controllers/application_stylesheet_controller.rb
@@ -30,35 +30,34 @@ class ApplicationStylesheetController < ApplicationController
     @c_b_w2 = '#797b7b'
     @c_b_w3 = '#a5a7a7'
     @c_b_w4 = '#d2d3d3'
-    # @c_b_w5 is simply 'white' in CSS so we don't need a variable for it here
+    
     @c_b_k1 = '#191c1c'
     @c_b_k2 = '#131515'
     @c_b_k3 = '#0c0e0e'
     @c_b_k4 = '#060707'
-    @c_b_k5 = '#030404'
     
     @c_a = '#f8510b'
     @c_a_w1 = '#f9943c'
     @c_a_w2 = '#fbc66d'
     @c_a_w3 = '#fce89d'
     @c_a_w4 = '#fefcce'
-    # @c_a_w5 is simply 'white' in CSS so we don't need a variable for it here
+    @c_a_w5 = '#ffffff'
+    # @c_a_w5 is simply 'white' in CSS so we don't need a variable for it here (lies!)
     @c_a_k1 = '#c63509'
     @c_a_k2 = '#951e07'
     @c_a_k3 = '#630d04'
     @c_a_k4 = '#320302'
-    # @c_a_k5 is simply 'black' in CSS so we don't need a variable for it here
+    @c_a_k5 = '#000000'
 
-    @c_t = '#0fa5dd'
-    @c_t_w1 = '#3fb6e4'
-    @c_t_w2 = '#6fc9eb'
-    @c_t_w3 = '#9fdbf1'
-    @c_t_w4 = '#cfedf8'
-    # @c_t_w5 is simply 'white' in CSS so we don't need a variable for it here
-    @c_t_k1 = '#0c83b1'
-    @c_t_k2 = '#096385'
-    @c_t_k3 = '#064258'
-    @c_t_k4 = '#03212c'
-    @c_t_k5 = '#021117'
+    @c_t = '#597eaa'
+    @c_t_w1 = '#7a98bb'
+    @c_t_w2 = '#9bb2cc'
+    @c_t_w3 = '#bdcbdd'
+    @c_t_w4 = '#dee5ee'
+
+    @c_t_k1 = '#476588'
+    @c_t_k2 = '#354c66'
+    @c_t_k3 = '#243244'
+    @c_t_k4 = '#121922'
   end
 end
