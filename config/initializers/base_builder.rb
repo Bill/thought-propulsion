57a83d99ae1a94d89288c45493c538de9832ae98
Bill <bill.burcham@gmail.com>
2008-10-05 09:58:33 -0700

    Modified submodule.

---
+++ config/initializers/base_builder.rb
@@ -0,0 +1,7 @@
+AirBlade::AirBudd::DivBuilder.default_options.merge!({
+  :label_suffix => ''
+})
+AirBlade::AirBudd::FormBuilder.default_options.merge!({
+  :required_signifier => '*',
+  :label_suffix => ''
+})
--- config/initializers/form_builder.rb
+++
@@ -1,4 +0,0 @@
-AirBlade::AirBudd::FormBuilder.default_options.merge!({
-  :required_signifier => '*',
-  :label_suffix => '',
-})
