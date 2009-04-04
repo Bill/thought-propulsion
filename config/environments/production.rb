198520180d9e6f96ad770a78edd902f4563d121d
Bill <bill.burcham@gmail.com>
2009-04-04 13:45:27 -0700

    remove file reference to alleviate heroku compaint (can't access filesystem)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,13 @@
 # workaround for ImageScience (RubyInline) problem:
 #  http://groups.google.com/group/ec2-on-rails-discuss/browse_thread/thread/ae7c45c2205b21fe?hl=en
 # http://www.viget.com/extend/rubyinline-in-shared-rails-environments/
-temp = Tempfile.new('ruby_inline', '/tmp')  
-dir = temp.path  
-temp.delete  
-Dir.mkdir(dir, 0755)  
-ENV['INLINEDIR'] = dir 
+if Image::PROCESSOR == 'ImageScience'
+  temp = Tempfile.new('ruby_inline', '/tmp')
+  dir = temp.path
+  temp.delete
+  Dir.mkdir(dir, 0755)
+  ENV['INLINEDIR'] = dir
+end
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
