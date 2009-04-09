80e8c431bb2273ae4e1740b591a73b935b5f82a4
Bill <bill.burcham@gmail.com>
2009-04-09 10:25:36 -0700

    added CSS styles to code highlighter

--- db/fixtures/development/twips.rb
+++ db/fixtures/development/twips.rb
@@ -1,17 +1,18 @@
 # You must explicitly run with rake db:seed RAILS_ENV=development for this seed to be loaded
 require 'lorem'
 propeller = User.find_by_nickname 'propeller'
-(0..1).each do |n|
-  twip = Twip.seed( :title) do |t|
-    # need to make something differ in each record so it will be saved (by seed) so make 
-    # title progressively longer
-    t.title = Lorem::Base.new('chars', 5 + n).output
-    t.body = Lorem::Base.new('paragraphs', 2).output
-    t.public = true
-    t.author = propeller
-  end
-  twip.save!
+
+twip = Twip.seed( :title) do |t|
+  t.title = 'Hyperlinks'
+  t.body = <<END
+Here is a <a href='thoughtpropulsion.com'>textual hyperlink</a> right there.
+And here's an image:
+<a href='thoughtpropulsion.com'><img src='http://images.dev.twipl.com.s3.amazonaws.com/images/1/IMG_0260.jpg'></img></a>
+END
+  t.public = true
+  t.author = propeller
 end
+twip.save!
 
 twip = Twip.seed( :title) do |t|
   t.title = 'Code'
@@ -51,3 +52,15 @@ END
   t.author = propeller
 end
 twip.save!
+
+(0..1).each do |n|
+  twip = Twip.seed( :title) do |t|
+    # need to make something differ in each record so it will be saved (by seed) so make 
+    # title progressively longer
+    t.title = Lorem::Base.new('chars', 5 + n).output
+    t.body = Lorem::Base.new('paragraphs', 2).output
+    t.public = true
+    t.author = propeller
+  end
+  twip.save!
+end
