a801bc17cce879a9f0ecb4f1a5a2188a0abfeaa8
Bill <bill.burcham@gmail.com>
2009-04-09 07:32:30 -0700

    styled highlighted code in Twips; created a new fixture for development that has a Twip with some code

--- db/fixtures/development/twips.rb
+++ db/fixtures/development/twips.rb
@@ -12,3 +12,42 @@ propeller = User.find_by_nickname 'propeller'
   end
   twip.save!
 end
+
+twip = Twip.seed( :title) do |t|
+  t.title = 'Code'
+  t.body = <<END
+  Here is some Ruby:<pre><code class="ruby">
+  &lt;%= some_erb %&gt;
+  class ContactController &lt; ThoughtPropulsionApplicationController
+    def index
+      @vcard = {
+        :fn =&gt; 'Thought Propulsion',
+        # FIXME: url helper
+        :url =&gt; url_for( '/'),
+        :addresses =&gt; [
+          { :type =&gt; 'meeting', :street_address =&gt; '622 SE Grand Ave.', :locality =&gt; 'Portland', :region =&gt; 'Oregon', :postal_code =&gt; '97214', :country_name =&gt; 'USA'},
+          { :type =&gt; 'postal', :post_office_box =&gt; '19863', :locality =&gt; 'Portland', :region =&gt; 'Oregon', :postal_code =&gt; '97280', :country_name =&gt; 'USA'}
+        ],
+        :emails =&gt; [
+          { :type =&gt; 'inquiries', :address =&gt; 'propeller@thoughtpropulsion.com'}
+        ],
+        :telephones =&gt; [
+          {
+          :type =&gt; 'work',
+          :value =&gt; '503.720.2991'
+          }]
+      }
+    end
+  end
+  </code></pre>
+  Here is some HTML:<br />
+  <pre><code class="html">&lt;!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"&gt;
+  &lt;!-- comment --&gt;
+  &lt;ul class="vinyl"&gt;<br /> &lt;li&gt;&lt;img src="/assets/2007/12/4/R-150-18062-1192002432.jpg" width="55" alt="Altern8 - Evapor8" /&gt;<br /> 			&lt;span class="track"&gt;&lt;a href="http://www.discogs.com/release/18062"&gt;Evapor8&lt;/a&gt;&lt;/span&gt; <br /> 			&lt;span class="artist"&gt;Altern8&lt;/span&gt;<br /> 			&lt;span class="label"&gt;Network 1992&lt;/span&gt;&lt;/li&gt;<br /> &lt;li&gt;&lt;img src="/assets/2007/12/4/tmb03978.jpg" width="55" alt="Teebone - Down Wid Da Funk (The Sequel)" /&gt;<br /> 			&lt;span class="track"&gt;&lt;a href="http://www.discogs.com/release/171726"&gt;Down Wid Da Funk (The Sequel)&lt;/a&gt;&lt;/span&gt; <br /> 			&lt;span class="artist"&gt;Teebone&lt;/span&gt;<br /> 			&lt;span class="label"&gt;Riddim Track&lt;/span&gt;&lt;/li&gt;<br /> &lt;li&gt;&lt;img src="/assets/2006/12/11/cee-lo.jpg" width="55" alt="Cee-Lo - I'll Be Around" /&gt;<br /> 			&lt;span class="track"&gt;&lt;a href="http://www.discogs.com/release/218091"&gt;I'll Be Around&lt;/a&gt;&lt;/span&gt; <br /> 			&lt;span class="artist"&gt;Cee-Lo w/ Timbaland&lt;/span&gt;<br /> 			&lt;span class="label"&gt;Arista 2003&lt;/span&gt;&lt;/li&gt;<br />&lt;/ul&gt;</code></pre>
+  That is all.
+END
+  t.public = true
+  t.author = propeller
+end
+twip.save!
