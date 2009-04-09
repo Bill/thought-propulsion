8c3ad3219ce0388c30258e50f8a82286b34c8bd6
Bill <bill.burcham@gmail.com>
2009-04-09 06:31:46 -0700

    changed color terminology from base/analog/complement to simply a/b/c because the former doesn't actually describe reality

--- app/controllers/stylesheets_controller.rb
+++ app/controllers/stylesheets_controller.rb
@@ -5,59 +5,58 @@ class StylesheetsController < ApplicationController
   
   COLORS = {
     # color palettes
-    # read this as "c" for "color" followed by either "b" for base hue or "a" for the analog (neighbor)
-    # or 't' for the complement (user sparingly!).
-    # after that comes an optional specification describing the addition of black or white e.g.
+    # read this as "c" for "color" followed by either "a", "b" or "c" to select a hue family.
+    # After that comes an optional specification describing the addition of black or white e.g.
     # k1 is one "unit" toward black ("k" from the CMYK tradition); w4 is four "units" toward white
     # You might want to use http://slayeroffice.com/tools/color_palette/ to generate palettes.
     # The purpose of these variables, however is to allow you to change the palette
     # without having to search and replace everything in the CSS.
-    :c_b => '#1F2323',
-    :c_b_w1 => '#2c2c2c',
-    :c_b_w2 => '#797b7b',
-    :c_b_w3 => '#a5a7a7',
-    :c_b_w4 => '#d2d3d3',
+    :c_a => '#1F2323',
+    :c_a_w1 => '#2c2c2c',
+    :c_a_w2 => '#797b7b',
+    :c_a_w3 => '#a5a7a7',
+    :c_a_w4 => '#d2d3d3',
     
-    :c_b_k1 => '#191c1c',
-    :c_b_k2 => '#131515',
-    :c_b_k3 => '#0c0e0e',
-    :c_b_k4 => '#060707',
+    :c_a_k1 => '#191c1c',
+    :c_a_k2 => '#131515',
+    :c_a_k3 => '#0c0e0e',
+    :c_a_k4 => '#060707',
     
-    :c_a => '#f8510b',
-    :c_a_w1 => '#f9943c',
-    :c_a_w2 => '#fbc66d',
-    :c_a_w3 => '#fce89d',
-    :c_a_w4 => '#fefcce',
-    :c_a_w5 => '#ffffff',
-    # :c_a_w5 is simply 'white' in CSS
-    :c_a_k1 => '#c63509',
-    :c_a_k2 => '#951e07',
-    :c_a_k3 => '#630d04',
-    :c_a_k4 => '#320302',
-    :c_a_k5 => '#000000',
+    :c_b => '#f8510b',
+    :c_b_w1 => '#f9943c',
+    :c_b_w2 => '#fbc66d',
+    :c_b_w3 => '#fce89d',
+    :c_b_w4 => '#fefcce',
+    :c_b_w5 => '#ffffff',
+    # :c_b_w5 is simply 'white' in CSS
+    :c_b_k1 => '#c63509',
+    :c_b_k2 => '#951e07',
+    :c_b_k3 => '#630d04',
+    :c_b_k4 => '#320302',
+    :c_b_k5 => '#000000',
 
-    :c_t => '#7eadde',
-    :c_t_w1 => '#98bde5',
-    :c_t_w2 => '#b2ceeb',
-    :c_t_w3 => '#cbdef2',
-    :c_t_w4 => '#e5eff8',
+    :c_c => '#7eadde',
+    :c_c_w1 => '#98bde5',
+    :c_c_w2 => '#b2ceeb',
+    :c_c_w3 => '#cbdef2',
+    :c_c_w4 => '#e5eff8',
 
-    :c_t_k1 => '#658ab2',
-    :c_t_k2 => '#4c6885',
-    :c_t_k3 => '#324559',
-    :c_t_k4 => '#19232c'
+    :c_c_k1 => '#658ab2',
+    :c_c_k2 => '#4c6885',
+    :c_c_k3 => '#324559',
+    :c_c_k4 => '#19232c'
   }
 
-  def self.base_colors
-    subset( 'c_b')
-  end
-
-  def self.analogous_colors
+  def self.a_colors
     subset( 'c_a')
   end
 
-  def self.complementary_colors
-    subset( 'c_t')
+  def self.b_colors
+    subset( 'c_b')
+  end
+
+  def self.c_colors
+    subset( 'c_c')
   end
   
   layout false
--- app/controllers/swatches_controller.rb
+++ app/controllers/swatches_controller.rb
@@ -4,10 +4,9 @@ class SwatchesController < ApplicationController
   ORDER = %w(_k5 _k4 _k3 _k2 _k1 * _w1 _w2 _w3 _w4 _w5).collect{|suf| suf == '*' ? '' : suf}
   
   def index
-     # @base_colors = ORDER.collect{ | suf | key = "c_b#{suf}".to_sym; [key, StylesheetsController::base_colors[key] ] }
-     @base_colors = sort_colors( 'c_b', StylesheetsController::base_colors)
-     @analogous_colors = sort_colors( 'c_a', StylesheetsController::analogous_colors)
-     @complementary_colors = sort_colors( 'c_t', StylesheetsController::complementary_colors)
+     @a_colors = sort_colors( 'c_a', StylesheetsController::a_colors)
+     @b_colors = sort_colors( 'c_b', StylesheetsController::b_colors)
+     @c_colors = sort_colors( 'c_c', StylesheetsController::c_colors)
   end
   private
   def sort_colors( pre, colors)
