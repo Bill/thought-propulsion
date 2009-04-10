d8c5e8d4e940646b2469f0e6c11d155563c9c6d8
Bill <bill.burcham@gmail.com>
2009-04-10 07:48:44 -0700

    comment cleanup

--- vendor/plugins/authored/lib/authored.rb
+++ vendor/plugins/authored/lib/authored.rb
@@ -33,13 +33,9 @@ module Authored
     # Note that a object is "shared with" its author implicitly so we need to join to user
     # TODO: add conditions for sharing with other users
     named_scope :access_shared_with, lambda{ |viewer_openid| 
-      # we have to use :select to help Rails 2.1.0 not get confused about which attributes go w/ which table
-      # http://selfamusementpark.com/blog/2008/07/10/named_scope-joins-includes/
       if viewer_openid.blank?
-        # { :conditions => 'FALSE', :select => "#{table_name}.*", :joins => [:author] }
         { :conditions => 'FALSE', :joins => [:author] }
       else
-        # { :conditions => {:identity_url => viewer_openid }, :select => "#{table_name}.*", :joins => [:author] }
         { :conditions => {:identity_url => viewer_openid }, :joins => [:author] }        
       end
     }
@@ -50,10 +46,8 @@ module Authored
     # TODO: add conditions for sharing with other users
     named_scope :access_public_or_shared_with, lambda{ |viewer_openid | 
       if viewer_openid.blank?
-        # { :conditions => ["public = ?", true], :select => "#{table_name}.*"}
         { :conditions => ["public = ?", true]}
       else
-        # { :conditions => ["public = ? OR identity_url = ?", true, viewer_openid], :select => "#{table_name}.*", :joins => [:author] }
         { :conditions => ["public = ? OR identity_url = ?", true, viewer_openid], :joins => [:author] }
       end
     }
